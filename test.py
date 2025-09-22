f001f68f54edb363a70e5d46563102a5858f8470
1nkognit0 <rb12212@gmail.com>
2025-09-22 18:09:03 +0700

    gitignore

--- test.py
+++
@@ -1,9 +0,0 @@
-a = [63, 80, 62, 69, 71, 37, 12, 90, 19, 67]
-N = len(a)
-
-for i in range(N-1):
-    for j in range(N-1-i):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
-
-print(a)
